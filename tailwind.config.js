138db39e326d064181778c773135f97ae4c2d55c
Hao <74541600+haod98@users.noreply.github.com>
2021-07-31 13:58:18 +0200

    Add content and style

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  purge: [
+    '*.html',
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+    fontFamily: {
+      'hou': ['houschka-pro', 'ui-serif', 'Georgia']
+    }
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
